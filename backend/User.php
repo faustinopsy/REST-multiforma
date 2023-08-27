1276cc6f6867831c3779eda08a42bbe2075680c2
faustinopsy <rodrigohipnose@gmail.com>
2023-08-27 09:38:55 -0300

    modified:   backend/User.php

--- backend/User.php
+++ backend/User.php
@@ -3,7 +3,7 @@
 class User {
     private $id;
     private $nome;
-
+    private $senha;
     public function __construct() {
       
     }
@@ -27,7 +27,12 @@ class User {
     public function getType() {
         return 'User';
     }
-
+    public function setSenha($senha) {
+        $this->senha = password_hash($senha, PASSWORD_DEFAULT)
+    }
+    public function getSenha() {
+        return $this->senha;
+    }
     public function toArray() {
         return ['id' => $this->getId(), 'nome' => $this->getNome(), 'type' => $this->getType()];
     }
