d92486770313ca29d1216b04782de202248141d8
faustinopsy <rodrigohipnose@gmail.com>
2023-08-27 17:14:14 -0300

    modified:   backend/UserManager.php

--- backend/UserManager.php
+++ backend/UserManager.php
@@ -7,7 +7,9 @@ use App\User\User;
 class UserManager {
 
     protected $model;
-    private $tokens = [
+	
+	
+	private $tokens = [
         'd09f7c370a88944e1e3d233f285e533dbf0736040aa79de1a2e8a8ffb570b6cf',
         'ae350ddaa07597c8eecf02e1f60da88014caef07da5fb547fecdb5f3e15d8cf8',
         '5f9c96696dcec2aafc9e6f9017a433088fdaa110388c70ad02f20b91e6a1550c',
@@ -21,100 +23,80 @@ class UserManager {
         '51753a0769fc6f4e53d5790df9b42325139167778e35ab9ef86bf74cba020554',
         '8f396973867b1260611632e49a315ea3cef3247168e280e4e0dbc67f7930c9cc',
     ];
-    private $database = [
-        ['id'=>1,'nome'=>'Teste 1 array 1'],
-        ['id'=>2,'nome'=>'Teste 2 array 2'],
-        ['id'=>3,'nome'=>'Teste 3 array 3'],
-        ['id'=>4,'nome'=>'Teste 4 array 4'],
-        ['id'=>5,'nome'=>'Teste 5 array 5'],
-        ['id'=>6,'nome'=>'Teste 6 array 6'],
-        ['id'=>7,'nome'=>'Teste 7 array 7'],
-        ['id'=>8,'nome'=>'Teste 8 array 8'],
-        ['id'=>9,'nome'=>'Teste 9 array 9'],
-        ['id'=>10,'nome'=>'Teste 10 array 10'],
-    ];
-    
-
     public function __construct() {
-
+        $this->model = new Model();
     }
-
 
     public function getUserById($id) {
-        foreach ($this->database as $userData) {
-            if ($userData['id'] == $id) {
-                $user = new User();
-                $user->setId($userData['id']);
-                $user->setNome($userData['nome']);
-                return $user->toArray();
-            }
+        $data = $this->model->read('users', ['id' => $id]);
+        if (!empty($data)) {
+            $users=[];
+            $user = new User();
+            $user->setId($data[0]['id']);
+            $user->setNome($data[0]['nome']);
+            $users=['id'=>$user->getId(),'nome'=>$user->getNome()];
+        return $users;
+        } else {
+            return false;
         }
-    
-        return false;
     }
-    
 
     public function getAllUsers() {
+        $data = $this->model->read('users');
         $users = [];
 
-        foreach ($this->database as $userData) {
+        foreach ($data as $userData) {
             $user = new User();
             $user->setId($userData['id']);
             $user->setNome($userData['nome']);
-            $users[] = $user->toArray();
+            $users[]=['id'=>$user->getId(),'nome'=>$user->getNome(),'type'=>$user->getType()];
         }
+
         return $users;
     }
 
     public function createUser($data) {
-        $resultado= $this->database;
+        $resultado= $this->model->read('users', ['nome' => $data['nome']]);
         if(!empty($resultado)){
             if($resultado[0]["nome"]==$data['nome']){
                 return false;
             }
         }
-        if(!empty($data)){
-            $user = new User();
-            $user->setNome($data["nome"]);
-            $maxId = max(array_column($this->database, 'id'));
-            $newId = $maxId + 1;
-            $this->database[] = ['id' => $newId, 'nome' => $data["nome"]];
-            return true;
-        }else{
-            return false;
-        }
-        
-       
+        $user = new User();
+        $user->setNome($data["nome"]);
+        return $this->model->create('users', ['nome' => $user->getNome()]);
     }
 
     public function updateUser($id, $data) {
-        foreach ($this->database as &$userData) {
-            if ($userData['id'] == $id) {
-                $userData['nome'] = $data["nome"];
-                $user = new User();
-                $user->setId($id);
-                $user->setNome($data["nome"]);
-                return $user;
-            }
+        $resultado=$this->getUserById($id);
+        if(!$resultado){
+            return false;
         }
-        return false;
+        $user = new User();
+        $user->setId($id);
+        $user->setNome($data["nome"]);
+        $sucesso= $this->model->update('users', ['nome' => $user->getNome()], ['id' => $user->getId()]);
+        if(!$sucesso){
+            return false;
+        }
+        return true;
     }
 
     public function deleteUser($id) {
-        foreach ($this->database as $index => $userData) {
-            if ($userData['id'] == $id) {
-                unset($this->database[$index]);
-                return true;
-            }
+        $resultado=$this->getUserById($id);
+        if(!$resultado){
+            return false;
         }
-        return false;
+        $user = new User();
+        $user->setId($id);
+        return $this->model->delete('users', ['id' => $user->getId()]);
     }
+
     public function generateToken() {
         //return bin2hex(openssl_random_pseudo_bytes(16));
-        return $this->tokens[rand(1,11)];
+        return $this->tokens[rand(1,12)];
     }
     public function isValidToken($token) {
         return in_array($token, $this->tokens);
     }
-    
 }
